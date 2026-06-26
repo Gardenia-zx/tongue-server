88edcf778c62f26caee6238ddac08183448b4fe5
Gardenia-zx <3469959715@qq.com>
2026-06-26 17:28:18 +0800

    feat(agent-chat): add conversation repository

---
+++ src/main/java/com/tongue/server/agentchat/v2/AgentConversationRepository.java
@@ -0,0 +1,9 @@
+package com.tongue.server.agentchat.v2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AgentConversationRepository extends JpaRepository<AgentConversationEntity, Long> {
+    Optional<AgentConversationEntity> findByUserIdAndConversationId(Long userId, String conversationId);
+}
