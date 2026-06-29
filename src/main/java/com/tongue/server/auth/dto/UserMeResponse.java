0d160ebc0229e740a11ea8003a46c486b254cd3b
Gardenia-zx <3469959715@qq.com>
2026-06-29 09:45:24 +0800

    feat(profile): expose profile center fields

--- src/main/java/com/tongue/server/auth/dto/UserMeResponse.java
+++ src/main/java/com/tongue/server/auth/dto/UserMeResponse.java
@@ -2,6 +2,8 @@ package com.tongue.server.auth.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.time.LocalDate;
+
 public class UserMeResponse {
     @JsonProperty("user_id")
     public Long userId;
@@ -10,8 +12,60 @@ public class UserMeResponse {
     public String nickname;
     public String gender;
     public Integer age;
+
+    @JsonProperty("birth_date")
+    public LocalDate birthDate;
+
+    public String email;
+
     @JsonProperty("avatar_file_id")
     public Long avatarFileId;
+
+    @JsonProperty("avatar_url")
+    public String avatarUrl;
+
     @JsonProperty("health_focus")
     public String healthFocus;
+
+    @JsonProperty("profile_note")
+    public String profileNote;
+
+    @JsonProperty("height_cm")
+    public Double heightCm;
+
+    @JsonProperty("weight_kg")
+    public Double weightKg;
+
+    @JsonProperty("sleep_hours")
+    public Double sleepHours;
+
+    @JsonProperty("exercise_frequency")
+    public String exerciseFrequency;
+
+    @JsonProperty("dietary_preference")
+    public String dietaryPreference;
+
+    @JsonProperty("answer_detail_level")
+    public String answerDetailLevel;
+
+    @JsonProperty("use_health_profile")
+    public Boolean useHealthProfile;
+
+    @JsonProperty("use_history_reports")
+    public Boolean useHistoryReports;
+
+    @JsonProperty("use_long_term_memory")
+    public Boolean useLongTermMemory;
+
+    @JsonProperty("tongue_reminder_enabled")
+    public Boolean tongueReminderEnabled;
+
+    @JsonProperty("tongue_reminder_time")
+    public String tongueReminderTime;
+
+    @JsonProperty("sleep_reminder_enabled")
+    public Boolean sleepReminderEnabled;
+
+    @JsonProperty("sleep_reminder_time")
+    public String sleepReminderTime;
 }
